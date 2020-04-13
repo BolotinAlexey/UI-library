52133434e6c3a39b9846995664cd22942bcd6b6c
AlexeyB <62134373+samkiral@users.noreply.github.com>
2020-04-13 04:26:57 +0300

    add files and changes index.html

---
+++ styles/burger.js
@@ -0,0 +1,17 @@
+let burger = document.getElementsByClassName("burger")[0];
+let menu = document.getElementsByClassName("nav-menu")[0];
+burger.addEventListener("click", showMenu);
+
+function showMenu() {
+  burger.style.display = "none";
+  menu.style.display = "flex";
+  let cross = document.getElementsByClassName("x")[0];
+  cross.style.display = "block";
+  cross.addEventListener('click', openBurger);
+}
+
+function openBurger() {
+  this.style.display = "none"
+  burger.style.display = "block";
+  menu.style.display="none";
+}
