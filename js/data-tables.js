d2dd29ad246565f46223267137a4a2a0fdfaccad
AlexeyB <62134373+samkiral@users.noreply.github.com>
2020-04-20 18:05:33 +0300

    выполнено 2е задание 4й домашки (сортировка)

--- js/data-tables.js
+++ js/data-tables.js
@@ -1,36 +1,85 @@
 function DataTable(config, data) {
-  let widthTable = config.columns.length;
-  let hightTable = users.length;
-  let root = document.getElementById(config.parent);
-  let table = createAndInnerElement("table", root, "");
-  let thead = createAndInnerElement("thead", table, "");
-  let trHead = createAndInnerElement("tr", thead, "");
-
+  let countSortable = 0;
+  let currentData = data;
+  /*начальная инициализация состояния кнопок сортировки*/
+  let sortState = new Map();
   config.columns.forEach(thElement => {
-    let th = createAndInnerElement("th", trHead, thElement.title);
-    if (thElement.type === 'number') {
-      th.className = "align-right";
+    if (thElement.sortable) {
+      sortState.set(thElement, 1);
+      countSortable++;
     }
   });
+  renderTable();
 
-  let tbody = createAndInnerElement("tbody", table, "");
-  let index = 0;
-  data.forEach(trElement => {
-    let trBody = createAndInnerElement("tr", tbody, "");
+  function renderTable() {
+    let root = document.getElementById(config.parent);
+    let table = createAndInnerElement("table", root, "");
+    let thead = createAndInnerElement("thead", table, "");
+    let trHead = createAndInnerElement("tr", thead, "");
+
+    /*прорисовка шапки*/
     config.columns.forEach(thElement => {
-      let stringValue = thElement.value;
-      let td = (stringValue === '_index') ? createAndInnerElement("td", trBody, ++index)
-        : createAndInnerElement("td", trBody, trElement[stringValue]);
+      let th = createAndInnerElement("th", trHead, thElement.title);
+      if (thElement.sortable) insertButtons(th, thElement, sortState);
       if (thElement.type === 'number') {
-        td.className = "align-right";
+        th.className = "align-right";
       }
     });
-    // createAndInnerElement("td",trBody,++index);
-    // createAndInnerElement("td",trBody,trElement.name);
-    // createAndInnerElement("td",trBody,trElement.surname);
-    // let age= createAndInnerElement("td",trBody,trElement.age);
-    // age.className="align-right";
-  });
+
+    /*прорисовка тела таблицы*/
+    let tbody = createAndInnerElement("tbody", table, "");
+    let index = 0;
+    currentData.forEach(trElement => {
+      let trBody = createAndInnerElement("tr", tbody, "");
+      config.columns.forEach(thElement => {
+        let stringValue = thElement.value;
+        let td = (stringValue === '_index') ? createAndInnerElement("td", trBody, ++index)
+          : createAndInnerElement("td", trBody, trElement[stringValue]);
+        if (thElement.type === 'number') {
+          td.className = "align-right";
+        }
+      });
+    });
+
+    function chooseSort(element, newState) {
+      if (newState === 1) return data;
+      let newUsers = data.slice(0);
+      newUsers.sort((a, b) => {
+        if (typeof(a[element.value])==="number") {
+          return (1 - newState) * (a[element.value] - b[element.value]);
+        }
+        else return (1 - newState)*
+          ((a[element.value].toLowerCase() < b[element.value].toLowerCase())?1:-1);
+      });
+      return newUsers;
+    }
+
+    function resort(element, sortState) {
+      /*инкреминируем циклически состояние сортировки колонки нажатой кнопки
+      * остальные кнопки сбрасываются в начальное состояние*/
+      let newState = (sortState.get(element) + 1) % (countSortable + 1);
+      config.columns.forEach(thElement => {
+        if (thElement.sortable) {
+          sortState.set(thElement, 1);
+        }
+      });
+      sortState.set(element, newState);
+      currentData = chooseSort(element, newState);
+      table.remove();
+      renderTable();
+    }
+
+    function insertButtons(th, element, sortState) {
+      let button = createAndInnerElement("button", th, "");
+      let classNameI = "fas " + ((sortState.get(element) === 0) ? "fa-sort-down" :
+        (sortState.get(element) === 1) ? "fa-sort" : "fa-sort-up");
+      let i = createAndInnerElement("i", button, "");
+      i.className = classNameI;
+      i.addEventListener("click", function () {
+        resort(element, sortState)
+      });
+    }
+  }
 }
 
 function createAndInnerElement(tagString, parentTag, inner) {
@@ -45,8 +94,8 @@ const config1 = {
   columns: [
     {title: '№', value: '_index'},
     {title: 'Имя', value: 'name'},
-    {title: 'Фамилия', value: 'surname'},
-    {title: 'Возраст', value: 'age', type: 'number'},
+    {title: 'Фамилия', value: 'surname', sortable: true},
+    {title: 'Возраст', value: 'age', type: 'number', sortable: true},
   ]
 };
 
