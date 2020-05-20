85a6faba74fcc01e887308f49292672746d8c2d7
AlexeyB <62134373+samkiral@users.noreply.github.com>
2020-05-20 07:51:59 +0300

    выполнено задание №5 (сделана обработка неправильного ввода)

--- my-input.js
+++ my-input.js
@@ -1,7 +1,7 @@
 Vue.component('my-input', {
   props: {
     title: String,
-    placeholderr: String,
+    placeholder: String,
     required: {
       type: Boolean,
       default: false
@@ -10,32 +10,47 @@ Vue.component('my-input', {
       type: String,
       default: 'text'
     },
+    err: {
+      type: String,
+      default: ''
+    },
+    pattern:{
+      type: String,
+      default: ''
+    }
 
   },
   template: `
 <div>
     <label>{{title}}{{required?'*':''}}<br>
     <input :type= "type" 
-    :placeholder="placeholderr" 
+    :placeholder="placeholder" 
     :required="required"
-    :class="{invalid: isValid}"
-    v-model="inputValue"
-    @blur="validate"
-    @input="validate,$emit('input', $event.target.value)">
+    :class="{blur: isBlur}"
+    @blur="checkFocus"
+    @input="checkFocus"
+    @change="validate"
+   >
+    <div 
+    class="error"
+    :class="{hide:isHide}">{{err}}</div>
     </label>
 </div>
 `,
   methods: {
-    validate: function (e) {
-      this.isValid = e.target.required && !e.target.value;
-      console.log(this.inputValue)
+    checkFocus: function (e) {
+      this.isBlur = (e.target.required && !e.target.value);
+      this.$emit('input', e.target.value);
     },
+    validate: function (e) {
+      this.isHide=(new RegExp(this.pattern)).test(String(e.target.value).toLowerCase());
+    }
   },
 
   data: function () {
     return {
-      isValid: false,
-      inputValue: ''
+      isBlur:false,
+      isHide: true,
     }
   }
 });
