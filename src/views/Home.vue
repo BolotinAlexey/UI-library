e3d419318cd33ef50c5e07670ecec976feb12a12
AlexeyB <62134373+samkiral@users.noreply.github.com>
2020-05-26 11:46:56 +0300

    task1 completed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <!--        <img alt="Vue logo" src="../assets/logo.png">-->
+        <!--        <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+        <div v-for="s in sizes">
+            <MyButton
+                    @clickOn='proc'
+                    v-for="c in colors" :color=c :size=s
+                    :href="'https://google.com/'+s+'/'+c"
+            >Text of Button
+            </MyButton>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    // @ is an alias to /src
+    import HelloWorld from '@/components/HelloWorld.vue'
+    import MyButton from "@/components/MyButton";
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'Home',
+        components: {
+            HelloWorld,
+            MyButton,
+        },
+        data() {
+            return {
+                colors: ['primary', 'success', 'warning', 'danger', 'light', 'secondary', 'dark'],
+                sizes: ['exsmall', 'small', 'medium', 'large', 'exlarge'],
+                size: 'medium',
+                color: '',
+                href: '',
+            }
+        },
+        methods: {
+            proc(data) {
+                console.log(data);
+            }
+        }
+    }
 </script>
