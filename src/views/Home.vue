4a48ce78d44c4732d33a6d36285c4ab6f02e4436
AlexeyB <62134373+samkiral@users.noreply.github.com>
2020-05-26 20:58:44 +0300

    task3 intermediate result

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyModal>
+        <template v-slot:header>
+
+        </template>
+        <template v-slot:default>
+
+        </template>
+        <template v-slot:trigger>
+
+        </template>
+    </MyModal>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import MyModal from '@/components/MyModal.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+      MyModal
   }
 }
 </script>
