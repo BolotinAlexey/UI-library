e2554c22a92fe95abd161d9875f09380e7e35369
AlexeyB <62134373+samkiral@users.noreply.github.com>
2020-05-28 16:13:00 +0300

    Beginning create component DataTable.vue,but its need previous components

---
+++ src/components/DataTable.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div class="table">
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "DataTable"
+    }
+</script>
+
+<style scoped>
+
+</style>
