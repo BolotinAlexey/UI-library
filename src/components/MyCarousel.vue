b5a1b63ad6d15ab6f7778057cadf2bde113e1420
AlexeyB <62134373+samkiral@users.noreply.github.com>
2020-05-28 15:43:36 +0300

    create component MyCarousel

---
+++ src/components/MyCarousel.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="container">
+
+        <div class="frame">
+            <button class="leftButton"
+                    :class="{hide:current===images.length-1}"
+                    @click="current++">
+                <img src="../assets/left.png"  height="25"/>
+            </button>
+
+            <img :src="images[current]"  alt="">
+
+            <button class="rightButton"
+                    :class="{hide: current===0}"
+                    @click="current--">
+                <img src="../assets/right.png" height="25"/>
+            </button>
+        </div>
+
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'MyCarousel',
+    components: {
+    },
+    props: {
+        images: Array,
+    },
+    data() {
+        return {
+            current: 0,
+        };
+    },
+};
+</script>
+
+<style scoped lang="less">
+    @w: 320px;
+    @h: 240px;
+
+    .mix {
+        position: absolute;
+        cursor: pointer;
+        top: 50%;
+        background-color: rgba(0,0,0,0);
+        border: none;
+        padding: 0;
+        :hover, :active {
+            background-color: rgba(0,0,0,.6);
+            border: none;
+        }
+    }
+
+    .container{
+        height: @h;
+        width: @w;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    .frame {
+       display: block;
+    }
+
+    .leftButton {
+        .mix;
+        display: block;
+        left: 0;
+
+    }
+
+    .rightButton {
+        .mix;
+        display: block;
+        right: 0;
+    }
+
+    .hide {
+        display: none;
+    }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <MyCarousel :images="images">
+        </MyCarousel>
+    </div>
 </template>
 
-<script>
+<script lang="ts">
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
+import MyCarousel from '@/components/MyCarousel.vue';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'Home',
+    components: {
+        HelloWorld,
+        MyCarousel,
+    },
+    data() {
+        return {
+            images: ['https://loremflickr.com/320/240/dog',
+                'https://loremflickr.com/320/240/cat',
+                'https://loremflickr.com/320/240/horse',
+                'https://loremflickr.com/320/240/men',
+                'https://loremflickr.com/320/240/women'],
+        };
+    },
+
+};
 </script>
